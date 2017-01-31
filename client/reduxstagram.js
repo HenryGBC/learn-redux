2e222cb3aa064d0183e2c3346cf70a9d8fc3ef2f
HenryGBC <henry.bravo89@gmail.com>
2017-01-30 23:04:22 -0430

    First commit. First & Second Video

---
+++ client/reduxstagram.js
@@ -0,0 +1,13 @@
+// let's go!
+import React from 'react';
+
+import { render } from 'react-dom';
+
+//import css
+
+import css from './styles/style.styl';
+
+//Import components
+import Main from './components/Main'; 
+
+render(<Main/>, document.getElementById('root'));
