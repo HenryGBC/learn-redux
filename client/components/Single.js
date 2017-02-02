e8d58e4565cf6a75e5ce4b471bb5199b160bbc9a
HenryGBC <henry.bravo89@gmail.com>
2017-02-01 22:54:32 -0430

    3 and 4, Setup react-router init components

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,1 +1,14 @@
-Single.js
+import React from 'react';
+import { Link } from 'react-router';
+
+const Single = React.createClass({
+	render(){
+		return(
+			<div className="single-photo">
+				I'm the single
+			</div>
+		)
+	}
+});
+
+export default Single;
